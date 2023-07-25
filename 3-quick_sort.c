18e1561a0828471f50fb25f4954ae9289b80ce8e
Hassan Cissey <hcissey0@gmail.com>
2023-07-25 23:10:32 +0000

    Quick Sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,70 @@
 #include "sort.h"
 
 /**
+ * swap - swaps 2 numbers
+ * @a: pointer to the first one
+ * @b: pointer to the second one
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * partition - partition thr array using the
+ * lomuto partition scheme
+ * @array: the array
+ * @size: size of the array
+ * @lo: the first element
+ * @hi: the last element
+ *
+ * Return: index of the pivot
+ */
+int partition(int *array, size_t size, int lo, int hi)
+{
+	int pivot = array[hi];
+	int i = lo - 1, j;
+
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	i++;
+	swap(&array[i], &array[hi]);
+
+	return (i);
+}
+
+/**
+ * my_quick_sort - my modification to be able to print the array
+ *
+ * @array: the array
+ * @size: the size
+ * @lo: the first element
+ * @hi: the last element
+ */
+void my_quick_sort(int *array, size_t size, int lo, int hi)
+{
+	int p = 0;
+
+	if (lo >= hi || lo < 0 || array == NULL || size < 2)
+		return;
+
+	p = partition(array, size, lo, hi);
+	my_quick_sort(array, size, lo, p - 1);
+	my_quick_sort(array, size, p + 1, hi);
+
+}
+
+/**
  * quick_sort - Sorts an array using the quick sort algorithm
  *
  * @array: The array to be sorted
@@ -10,47 +74,6 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	my_quick_sort(array, size, size);
+	my_quick_sort(array, size, 0, size - 1);
 }
 
-/**
- * my_quick_sort - my modification to be able to print the array
- *
- * @array: the array
- * @size: the size
- * @os: the old size
- */
-void my_quick_sort(int *array, size_t size, size_t os)
-{
-	size_t i, j, tmp;
-	int pivot;
-
-	if (array == NULL || size <= 1)
-		return;
-	/* set pivot to last element */
-	pivot = array[size - 1];
-	i = 0;
-	j = 0;
-	while (i < size - 1)
-	{
-		if (array[i] <= pivot)
-		{
-			/* swap */
-			tmp = array[j];
-			array[j] = array[i];
-			array[i] = tmp;
-			j++;
-		}
-		i++;
-	}
-
-	/* swap pivot with current j */
-	tmp = array[j];
-	array[j] = array[size - 1];
-	array[size - 1] = tmp;
-	print_array(array, os);
-
-	/* do the recursion for the sub arrays */
-	my_quick_sort(array, j, os);
-	my_quick_sort(array + j + 1, size - j - 1, os);
-}
