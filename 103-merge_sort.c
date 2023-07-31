009365c8e8bf054c683a72e34754c7ec82348374
Hassan Cissey <hcissey0@gmail.com>
2023-07-31 06:34:50 +0000

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,51 +3,54 @@
 /**
  * merger - merges 2 sub arrays
  * @array: the array
+ * @size: size of the array
  * @tmp: the temp
- * @l: the left
  * @m: the middle
- * @r: the right
  */
-void merger(int *array, int *tmp, int l, int m, int r)
+void merger(int *array, size_t size, int *tmp, int m)
 {
-	int i, j, k;
+	int i = 0, j = m, k = 0;
 
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_array(array + l, m - l + 1);
-	printf("[right]: ");
-	print_array(array + m + 1, r - m);
+	print_array(array,  m);
 
-	for (i = l, j = m + 1, k = 0; k <= r - l; k++)
-	{
-		if (i <= m && (j > r || array[i] <= array[j]))
-			tmp[k] = array[i++];
-		else
-			tmp[k] = array[j++];
-	}
-	for (i = l, k = 0; i <= r; i++, k++)
-		array[i] = tmp[k];
+	printf("[right]: ");
+	print_array(array + m, size - m);
+
+	while (i < m && j < (int)size)
+		tmp[k++] = (array[i] < array[i]) ? array[i++] : array[j++];
+
+	while (i < m)
+		tmp[k++] = array[i++];
+
+	while (j < (int)size)
+		tmp[k++] = array[j++];
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = tmp[i];
+
 	printf("[Done]: ");
-	print_array(array + l, r - l +1);
+	print_array(tmp, size);
 }
 
 /**
  * my_merge_sort - my recursive merge sort
  * @array: the array
+ * @size: size of the array
  * @tmp: the temp
- * @l: the left index
- * @r: the right index
  */
-void my_merge_sort(int *array, int *tmp, int l, int r)
+void my_merge_sort(int *array, size_t size, int *tmp)
 {
 	int m;
 
-	if (r <= l)
+	if (size < 2)
 		return;
-	m = (l + r) / 2;
-	my_merge_sort(array, tmp, l, m);
-	my_merge_sort(array, tmp, m + 1, r);
-	merger(array, tmp, l, m, r);
+
+	m = size / 2;
+	my_merge_sort(array, m, tmp);
+	my_merge_sort(array + m, size - m, tmp);
+	merger(array, size, tmp, m);
 }
 
 /**
@@ -64,7 +67,7 @@ void merge_sort(int *array, size_t size)
 	tmp = malloc(sizeof(int) * size);
 	if (tmp == NULL)
 		return;
-	my_merge_sort(array, tmp, 0, size - 1);
+	my_merge_sort(array, size, tmp, size);
 
 	free(tmp);
 }
